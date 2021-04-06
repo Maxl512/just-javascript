8033d79df3799328f6d6cc700b11058cbd1e83a5
Maximo <maximo.learn.code@gmail.com>
2021-04-06 18:14:44 -0400

    Gun Game

--- tasks.js
+++ tasks.js
@@ -282,31 +282,214 @@ function deletePerson(index) {
 
 */
 let gun = document.querySelector("#gun");
+
 let ammo = document.querySelector("#ammo");
-const shot = () =>{
-    ammo.style.width = "150%";
-    ammo.style.height = "100%";
-    ammo.style.left ="730%";
-    ammo.style.visibility ="hidden";
-    ammo.style.transition ="width .2s linear, height .2s linear, left .4s ease-out, visibility .7s linear";
+
+let monster = document.querySelector("#monster");
+let monsterEyeL = document.querySelector("#eye-L");
+let monsterEyeR = document.querySelector("#eye-R");
+let monsterMouth = document.querySelector("#mouth");
+
+let shotOne = document.querySelector("#shotOne");
+let shotTwo = document.querySelector("#shotTwo");
+let shotThree = document.querySelector("#shotThree");
+
+let gunPosition = gun.getBoundingClientRect();
+let monsterPosition = monster.getBoundingClientRect();
+let topGun = gunPosition.top;
+let topMonster = monsterPosition.top;
+let XMonster = monsterPosition.right;
+
+let kills = document.querySelector("#kills");
+let killsCount = kills.textContent;
+killsCount = parseInt(killsCount);
+
+let score = document.querySelector("#score");
+let scoreCount = score.textContent;
+scoreCount = parseInt(scoreCount);
+
+let lives = document.querySelector("#lives");
+let livesCount = lives.textContent;
+livesCount = parseInt(livesCount);
+
+const shot = (time) =>{
+    let visible = ammo.getAttribute("hidden");
+    if(visible === null){
+        ammo.style.width = "150%";
+        ammo.style.height = "100%";
+        ammo.style.left ="1100px";
+        ammo.style.transition ="width .2s linear, height .2s linear, left .5s ease-out";
+        setTimeout(() => {
+            ammo.setAttribute("hidden", "");
+        }, 510);
+
+        killMonster(time);
+    } else{
+        if (time === 1) {
+            shotOne.style.backgroundColor = "#000";
+            shotOne.style.border = "1px solid #fff";
+            shotOne.style.color = "#eee";
+        } else if (time === 2){
+            shotTwo.style.backgroundColor = "#000";
+            shotTwo.style.border = "1px solid #fff";
+            shotTwo.style.color = "#eee";
+        } else if (time === 3){
+            shotThree.style.backgroundColor = "#000";
+            shotThree.style.border = "1px solid #fff";
+            shotThree.style.color = "#eee";
+        };
+    };
 };
 const reload =() =>{
+    shotOne.style.backgroundColor = "#fff";
+    shotOne.style.border = "1px solid #000";
+    shotOne.style.color = "#000";
+
+    shotTwo.style.backgroundColor = "#fff";
+    shotTwo.style.border = "1px solid #000";
+    shotTwo.style.color = "#000";
+
+    shotThree.style.backgroundColor = "#fff";
+    shotThree.style.border = "1px solid #000";
+    shotThree.style.color = "#000";
+
     ammo.style.transition ="none";
     ammo.style.width = "50%";
     ammo.style.height = "50%";
     ammo.style.left ="0%";
-    ammo.style.visibility ="visible";
-    
+
+    ammo.removeAttribute("hidden");
+};
+function killMonster(time){
+    if(topGun == topMonster && time == 1){        
+        monster.style.backgroundColor = "#660";
+        monster.style.top = "55%";
+        monster.style.borderBottom = "10px solid #020";
+        monster.style.borderTop="50px solid #020";
+        monster.style.borderRadius ="0% 0% 30% 30%";
+        monsterMouth.style.borderRadius = "0% 0% 0% 0%";
+        monsterMouth.style.backgroundColor = "#400";
+        monsterEyeL.style.backgroundColor="#fff";
+        monsterEyeR.style.backgroundColor="#fff";
+
+        monster.style.right = `${1440 - XMonster}px`;
+        monster.style.animationName = "monsterRetirement";
+        monster.style.animationIterationCount = `${time}`;
+        monster.style.animationDuration = "2s";
+
+        setTimeout(() => {
+            monster.style.animationName = "monsterAttack";
+            monster.style.animationIterationCount = `${time}`;
+            monster.style.animationDuration = "2s";
+        }, 2010);
+   
+        shotOne.style.display = "none";
+        shotTwo.style.display = "inline-block";
+    } else if(topGun == topMonster && time == 2){
+        monster.style.right = `${1440 - XMonster}px`;
+   
+        monsterMouth.style.borderRadius = "50% 50% 0% 0%";
+        monsterMouth.style.backgroundColor = "#000";
+        monsterEyeL.style.backgroundColor="#300";
+        monsterEyeR.style.backgroundColor="#300"; 
+
+        monster.style.animationName = "monsterRetirement";
+        monster.style.animationIterationCount = `${time - 1}`;
+        monster.style.animationDuration = "2s";
+        monster.style.backgroundColor = "#600";
+
+        setTimeout(() => {
+            monster.style.top = "10%";
+            monster.style.animationName = "monsterAttack";
+            monster.style.animationIterationCount = `${time - 1}`;
+            monster.style.animationDuration = "2s";
+        }, 2010);
+
+        shotTwo.style.display = "none";
+        shotThree.style.display = "inline-block";
+
+    } else if(topGun == topMonster && time == 3){
+        monster.style.right = `${1440 - XMonster}px`;
+        monster.style.animation ="none";
+
+        monster.style.backgroundColor = "#fff";
+        monster.style.borderRadius = "50% 50% 0% 0%";
+        monster.style.borderBottom = "none";
+        monster.style.borderTop = "none";
+        monsterMouth.style.borderRadius = "50% 50% 0% 0%";
+        monsterMouth.style.height ="25px";
+        monsterMouth.style.backgroundColor = "#aaa";
+        monsterEyeL.style.backgroundColor="#aaa";
+        monsterEyeR.style.backgroundColor="#aaa";
+
+        monster.style.transition = "right 1s linear, top 1s linear, width 1s linear, height 1s linear";
+        
+        setTimeout(() => {
+            monster.style.top = "450px";
+            monster.style.right = "640px";
+            monster.style.width = "100px";
+            monster.style.height = "40px";
+
+            monsterEyeL.style.display = "none";
+            monsterEyeR.style.display = "none";
+            monsterMouth.style.display ="none";  
+        }, 1000);
+
+        setTimeout(() => {
+            monster.style.display ="none";
+
+            killsCount += 1;
+            kills.innerHTML = killsCount;
+
+            scoreCount += 100;
+            score.innerHTML = scoreCount;
+   
+            shotThree.style.display = "none";
+            shotOne.style.display = "inline-block"
+
+            anotherOne();
+        }, 2000);
+    };
+}; 
+function anotherOne(){
+    monster.style.display = "block";
+    monster.style.position = "absolute";
+    monster.style.width = "200px";
+    monster.style.height = "180px";
+    monster.style.backgroundColor = "#070";
+    monster.style.borderTop = "50px solid #020";
+    monster.style.borderBottom = "10px solid #020";
+    monster.style.top = "10%";
+    monster.style.right = "10px";
+    monster.style.borderRadius = "0% 0% 30% 30%";
+
+    monsterEyeL.style.display = "inline-block";
+    monsterEyeL.style.backgroundColor = "#000";
+    monsterEyeL.style.borderTop = "5px solid #aaa";
+
+    monsterEyeR.style.display = "inline-block";
+    monsterEyeR.style.backgroundColor = "#000";
+    monsterEyeR.style.borderTop = "5px solid #aaa";
+
+    monsterMouth.style.display ="inline-block";  
+    monsterMouth.style.backgroundColor = "#700";
+    monsterMouth.style.borderRadius = "0% 0% 50% 50%";
+
+    monster.style.transition ="top .1s linear";
+
+    monster.style.animationName = "monsterAttack";
+    monster.style.animationIterationCount ="1";
+    monster.style.animationTimingFunction = "linear";
+    monster.style.animationDuration = "2s";
+    monster.style.animationDirection = "normal";
 };
 function moveGun(event, position){
-    let dir = event.which || event.keyCode;
-    
+    let dir = event.which || event.keyCode;  
     if(dir === 40){
-        position += 15; 
+        position += 25; 
         gun.style.top = `${position}%`;
     } else if (dir === 38){
-        position -= 15;
+        position -= 20;
         gun.style.top = `${position}%`;
     };
-
-};
+};  
